031865c169f80863a8eac075a6490b3567f7addd
Petrov Ilya <dnamix1@gmail.com>
2021-07-28 15:02:04 +0300

    adding connection between Lead Sources class and it's policy

--- app/Modules/Admin/Sources/Seeds/SourcesSeed.php
+++ app/Modules/Admin/Sources/Seeds/SourcesSeed.php
@@ -4,7 +4,26 @@
 namespace App\Modules\Admin\Sources\Seeds;
 
 
-class SourcesSeed
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class SourcesSeed extends Seeder
 {
 
+    public function run() {
+        DB::table('sources')->insert([
+            [
+                'title' => 'Facebook'
+            ],
+            [
+                'title' => 'Internet Commercials'
+            ],
+            [
+                'title' => 'YouTube Channel'
+            ],
+            [
+                'title' => 'Partners Program'
+            ]
+        ]);
+    }
 }
